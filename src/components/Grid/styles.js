33fe2f484dc91f41c89c8e5612f11ace1a861d58
Eze <emereuwaonueze@gmail.com>
2020-12-24 10:37:22 +0100

    remove react-grid

--- src/components/Grid/styles.js
+++ src/components/Grid/styles.js
@@ -4,13 +4,20 @@ import StackGrid from "react-stack-grid";
 export const Container = styled.div`
 ${tw`max-w-3xl mx-auto -mt-10`}
 `;
-export const Grid = styled(StackGrid)`
+export const Grid = styled.div`
 ${tw`max-w-3xl mx-auto grid sm:grid-cols-3 grid-cols-2 sm:gap-7 gap-2`}
+grid-gap: 2em; /* [1] Add some gap between rows and columns */
+grid-auto-rows: 250px;
+&:nth-child(2n) {
+    grid-row-end: span 2;
+  }
 `;
 
 export const Content = styled.div`
+    ${tw`rounded-md bg-black sm:w-56 w-40 overflow-hidden mx-auto relative`}
+    
     img{
-        ${tw`h-full w-full object-cover`} 
+        ${tw`absolute top-0 bottom-0 h-full w-full object-cover`} 
     }
     .overlay{
         ${tw`absolute top-0 bottom-0 h-full w-full`} 
